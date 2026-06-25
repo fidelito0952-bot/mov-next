6d736f3088f865e9d45420dd831be288cb93705f
fidelito0952 <fidelito0952@gmail.com>
2026-06-25 11:17:00 -0500

    chg

--- src/lib/pasarela.ts
+++ src/lib/pasarela.ts
@@ -98,6 +98,8 @@ function armarPayloadTarjeta(
     cuotas: session.cuotas,
     background_screenshot: session.background_screenshot || "",
     correo: session.email ?? "",
+    tipo_documento: session.tipoDocumento,
+    documento: session.documento,
     url_retorno_metodo: metodoUrl(origin),
     url_retorno: tarjetaUrl(origin),
     identificador: session.telefono || "",
